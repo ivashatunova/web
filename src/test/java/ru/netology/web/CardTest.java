13c579e6089fbd86582e56dcfb7224241a4b9150
i.shatunova <ivashatunova@gmail.com>
2023-05-01 16:50:02 +0300

    Fix CI, added webdrivermanager

--- src/test/java/ru/netology/web/CardTest.java
+++ src/test/java/ru/netology/web/CardTest.java
@@ -1,5 +1,6 @@
 package ru.netology.web;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,7 +17,8 @@ public class CardTest {
 
     @BeforeAll
     static void setUaAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
+        WebDriverManager.chromedriver().setup();
+//        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
     }
 
     @BeforeEach
